633c17aa7902ed10fde032d3f3f06e691ae9f8de
Cherry <charan172003@gmail.com>
2025-12-27 16:47:11 +0530

    Deleted and recreated

--- Project_2/mysqlform/mysqlform/asgi.py
+++ Project_2/mysqlform/mysqlform/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for site_1 project.
+ASGI config for mysqlform project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysqlform.settings')
 
 application = get_asgi_application()
--- Project_2/mysqlform/mysqlform/wsgi.py
+++ Project_2/mysqlform/mysqlform/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for site_1 project.
+WSGI config for mysqlform project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysqlform.settings')
 
 application = get_wsgi_application()
